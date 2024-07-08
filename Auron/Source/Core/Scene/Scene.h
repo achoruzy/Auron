68c0dd523307dc3d1ce0ea68c37f7c1caaa8c507
achoruzy <achoruzy@gmail.com>
2024-07-08 21:11:43 +0200

    update

---
+++ Auron/Source/Core/Scene/Scene.h
@@ -0,0 +1,19 @@
+// Copyright (C) Auron Project by Arkadiusz Choruzy
+// All rights reserved.
+
+#pragma once
+
+#include "SceneObject.h"
+#include <vector>
+
+namespace Auron { 
+
+    class Scene
+    {
+    private:
+        std::vector<SceneObject> SceneObjects;
+    public:
+        Scene();
+        ~Scene();
+    };
+}
